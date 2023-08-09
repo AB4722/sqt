81e2779460ed176c5c5da303837ffb9dc79d4905
AB4722 <ab4722@ic.ac.uk>
2023-08-09 11:12:21 +0100

    TCom
    starting point as game seems to work at this point

--- web-app/Score.js
+++ web-app/Score.js
@@ -21,11 +21,27 @@ const Score = {};
  * @returns {Score.Score} The new game.
  */
 Score.new_score = function () {
-    return 0;
+    return {
+        score: 0,
+        lines_cleared: 0
+    };
 };
-
+/**
+ * Calculates the current level based on the number of lines cleared.
+ * The level increases by 1 for every 10 lines cleared.
+ *
+ * @function
+ * @memberof Score
+ * @param {Score.Score} score - The Score object containing information about the game score.
+ * @returns {number} The current level calculated based on lines cleared.
+ * @example
+ * // Assuming scoreObj has { score: 200, lines_cleared: 23 }
+ * const currentLevel = Score.level(scoreObj); // Returns 3
+ */
 Score.level = function (score) {
-    return 1;
+    const linesCleared = score.lines_cleared;
+    return Math.floor(linesCleared / 10) + 1;
 };
 
 export default Object.freeze(Score);
+
